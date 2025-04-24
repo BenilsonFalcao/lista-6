de6fb8d8bf95d26927d7a9d2855ee0971bca3bc9
BenilsonFalcao <benilson.25104708360010@faeterj-rio.edu.br>
2025-04-24 10:59:38 -0300

    Update and rename README.md to lista.c

---
+++ lista.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*Faça um programa que leia
+um número inteiro x e, em seguida, solicite
+ao usuário outros 50 valores inteiros. Ao
+final, o programa deve exibir o total de
+múltiplos de x fornecidos.*/
+
+int x, n, quantInteiros, divisor
+do
+{
+  print (defina o valor: )
+  scanf ("%d" &n)
+}  
+while (quantInteiros<=50)
+  for (quantInteiros=1; quantInteiros<=50; quantInteiros++)
+
+if
+  n/x = 0
